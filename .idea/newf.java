5edc246b17ba747c724edcf3e1670cf972505aef
Klavisi <klavisinaklavisi@gmail.com>
2025-10-17 11:16:57 +0500

    commit

--- .idea/newf.java
+++ .idea/newf.java
@@ -1,6 +1,6 @@
 public class HelloRodl {
     public static void main(String[] args) {
-        String name = "Cryденг";
+        String name = "dgfhjkj";
         int age = 20;
         System.out.println("Привет, мир! Мне " + age + " лет.");
     }
